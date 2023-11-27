7a106d20b9ae49f4e1679d1594b2108c8a095e5b
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-11-28 00:17:42 +0900

    :sparkles: Feat: S3Config.java 및 s3.yml 변경

--- src/main/java/com/example/streaming_service/global/config/aws/S3Config.java
+++ src/main/java/com/example/streaming_service/global/config/aws/S3Config.java
@@ -7,15 +7,13 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application-s3.yml")
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.accesskey}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
-    @Value("${cloud.aws.credentials.secretkey}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
