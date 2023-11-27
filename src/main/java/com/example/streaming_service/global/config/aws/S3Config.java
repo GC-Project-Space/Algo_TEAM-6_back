8d78af5d8942e983ae8cf97418bb67adc80cfb4e
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-11-27 23:41:34 +0900

    :sparkles: Feat: S3Config.java 수정

--- src/main/java/com/example/streaming_service/global/config/aws/S3Config.java
+++ src/main/java/com/example/streaming_service/global/config/aws/S3Config.java
@@ -11,9 +11,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accesskey}")
     private String accessKey;
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretkey}")
     private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
